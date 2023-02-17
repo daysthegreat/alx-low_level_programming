571eed513e63764a500bbfe223a322edce99f660
daysthegreat <davisagbevegh@gmail.com>
2023-02-17 07:28:40 -0800

    print alphabet in lovercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+
 /**
-*main - print the alphabets in lowercase
-*
-*Return: Always 0
-*/
+ * main - Print the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 char letter;
-for (letter = 'a'; letter <= 'z' letter++)
+
+for (letter = 'a'; letter <= 'z'; letter++)
 putchar(letter);
-putchar('n\');
+
+putchar('\n');
+
 return (0);
+}
