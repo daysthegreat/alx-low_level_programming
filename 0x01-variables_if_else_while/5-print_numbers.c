847b29f2ba6e64dc5e188b0a0d1b24532def8cba
daysthegreat <davisagbevegh@gmail.com>
2023-02-17 07:57:23 -0800

    print alphabets except q & e

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - assign random number to variable
+ *print number stored in the variable n
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
