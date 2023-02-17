18cf390e3c7ce14f20b124fbff2f9d85d6006e2c
daysthegreat <davisagbevegh@gmail.com>
2023-02-17 07:32:16 -0800

    print alphabet in lowercase then uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - print the alphabets in lowercase, then in uppercase
+*
+*Return: Always 0
+*/
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z' letter++)
+putchar(letter);
+for (letter = 'A'; letter <= 'A' letter++)
+putchar(letter);
+putchar('\n');
+return (0);
+
