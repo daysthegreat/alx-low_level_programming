e41cbcc606717ac8e88f6009cb3d18ff7cda7190
daysthegreat <davisagbevegh@gmail.com>
2023-02-21 19:26:43 +0000

    prints the last digit of a number.

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,7 +9,7 @@
 int print_last_digit(int n)
 
 {
-	int a;        
+	int a;
 
 	if (n < 0)
 		n = -n;
@@ -18,9 +18,9 @@ int print_last_digit(int n)
 
 	if (a < 0)
 		a = -a;
-	
+
 	_putchar(a + '0');
-	
+
 	return (a);
 
 }
