40c50f01c69a9c753ed55495cdc8c256b920e559
daysthegreat <davisagbevegh@gmail.com>
2023-02-22 04:42:49 -0800

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,35 +1,29 @@
 #include <stdio.h>
 
 /**
- *  * main - finds and prints the sum of the even-valued terms
- *   * followed by a new line
- *    * Return: Always 0 (Success)
+ * main - finds and prints the sum of even-valued terms
+ * followed by a new line
+ * Return: Always 0 (success)
  */
-
 int main(void)
-
 {
-	int i;
+	int a;
+	unsigned long int b, c, next, sum;
 
-	unsigned long int j, k, next, sum;
-	j = 1;
-	k = 2;
+	b = 1;
+	c = 2;
 	sum = 0;
-	for (i = 1; i <= 33; ++i)
 
+	for (a = 1; a <= 89; ++a)
 	{
-		if (j < 4000000 && (j % 2) == 0)
-
+		if (b < 4000000 && (b % 2) == 0)
 		{
-			sum = sum + j;
+			sum = sum + b;
 		}
-
-		next = j + k;
-		j = k;
-		k = next;
-
+		next = b + c;
+		b = c;
+		c = next;
 	}
-
 	printf("%lu\n", sum);
 	return (0);
 }
