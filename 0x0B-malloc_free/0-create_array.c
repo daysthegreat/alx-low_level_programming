a2f1fb0d1849f0854e914f399f90ba6f20a35183
daysthegreat <davisagbevegh@gmail.com>
2023-03-13 19:55:13 +0000

    C - malloc, free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,27 +1,38 @@
-#include "holberton.h"
+#include "main.h"
+		
 #include <stdlib.h>
-
+		
 /**
- * create_array - create an array of chars, and initialize with specific char
- * @size: size of array
- * @c: specific char
- * Return: char pointer to malloc created memory address or NULL if error
+ * create_array - create array of size size and assign char c		
+ * @size: size of array		
+ * @c: char to assign
+ * Description: creat array of size size and assign char c
+ * Return: pointer to array, NULL if fail
+ *
  */
-
+		
 char *create_array(unsigned int size, char c)
+		
 {
-	char *a;
+		
+	char *str;
+		
 	unsigned int i;
+		
 
-	if (size == 0)
+		
+	str = malloc(sizeof(char) * size);
+		
+	if (size == 0 || str == NULL)
+		
 		return (NULL);
+		
 
-	a = malloc(size * sizeof(*a));
-	if (a == NULL)
-		return (NULL);
-
+		
 	for (i = 0; i < size; i++)
-		a[i] = c;
-
-	return (a);
+		
+		str[i] = c;
+		
+	return (str);
+		
 }
--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,30 +1,53 @@
-nclude "holberton.h"
+#include <stdio.h>
+		
 #include <stdlib.h>
-
-/**
- * _strdup - Duplicate a string using malloc
- * @str: string to duplicate
- * Return: Pointer to a the new duped string
+		
+#include "main.h"
+		
+/**		
+ * _strdup - duplicate to new memory space location		
+ * @str: char
+ * Return: 0	
  */
-
+		
 char *_strdup(char *str)
+		
 {
-	char *a;
-	int i, c;
+		
+	char *aaa;
+		
+	int i, r = 0;
+		
 
+		
 	if (str == NULL)
+		
 		return (NULL);
+		
+	i = 0;
+		
+	while (str[i] != '\0')
+		
+		i++;
+		
 
-	for (i = 0; str[i] != '\0'; i++)
-		;
+		
+	aaa = malloc(sizeof(char) * (i + 1));
+		
 
-	a = malloc(i * sizeof(*a) + 1);
-	if (a == NULL)
+		
+	if (aaa == NULL)
+		
 		return (NULL);
+		
 
-	for (c = 0; c < i; c++)
-		a[c] = str[c];
-	a[c] = '\0';
+		
+	for (r = 0; str[r]; r++)
+		
+		aaa[r] = str[r];
+		
 
-	return (a);
+		
+	return (aaa);
+		
 }
