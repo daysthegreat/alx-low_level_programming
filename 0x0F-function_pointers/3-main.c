1b03e4aebbcf99dcdac46e428823e0a6150a48d1
daysthegreat <davisagbevegh@gmail.com>
2023-03-23 15:07:57 +0000

    corrections for 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -33,8 +33,7 @@ printf("Error\n");
 exit(99);
 }
 
-if ((*op == '/' && num2 == 0) ||
- (*op == '%' && num2 == 0))
+if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 {
 printf("Error\n");
 exit(100);
