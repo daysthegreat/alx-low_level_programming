4128e7d0e259f33988abd9b89d6ff6f94058968b
daysthegreat <davisagbevegh@gmail.com>
2023-04-04 15:53:23 +0000

    a function that prints a listint_t linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,7 +3,6 @@
 /**
  * free_listp - frees a linked list
  * @head: head of a list.
- *
  * Return: no return.
  */
 void free_listp(listp_t **head)
@@ -26,7 +25,6 @@ void free_listp(listp_t **head)
 /**
  * print_listint_safe - prints a linked list.
  * @head: head of a list.
- *
  * Return: number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
