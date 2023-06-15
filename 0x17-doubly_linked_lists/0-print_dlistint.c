c88784855b05e531c9c51bd4feef265ee9d590b6
daysthegreat <davisagbevegh@gmail.com>
2023-06-15 14:01:57 +0000

    a function that prints all the elements of a dlistint_t list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - Print all the values of each node in a `dlistint_t` list
+ * @h: head pointer in doubly linked list
+ * Return: Number of nodes in LL
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *current = h;
