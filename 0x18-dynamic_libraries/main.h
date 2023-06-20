94f51b41896be763e9a5710bba4d55d2ac19db79
daysthegreat <davisagbevegh@gmail.com>
2023-06-20 15:35:04 +0000

    main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,20 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
